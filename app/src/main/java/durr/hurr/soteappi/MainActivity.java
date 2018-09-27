537b97950dc9fb22f5673a14b4369eeabea27610
Jussiprojekti <kaasukammio@gmail.com>
2018-09-27 14:07:49 +0300

    kesken

--- app/src/main/java/durr/hurr/soteappi/MainActivity.java
+++ app/src/main/java/durr/hurr/soteappi/MainActivity.java
@@ -3,12 +3,15 @@ package durr.hurr.soteappi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.time.LocalDate;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Day tanaan = new(Day(LocalDate))
     }
 
 }
