bb4d2805ce704933c78182c2da9e40698c67d437
Dartti88 <dartti88@hotmail.com>
2018-10-01 10:56:39 +0300

    Radiobuttons

--- app/src/main/java/durr/hurr/soteappi/MainActivity.java
+++ app/src/main/java/durr/hurr/soteappi/MainActivity.java
@@ -1,18 +1,43 @@
 package durr.hurr.soteappi;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
 
 import java.time.LocalDate;
 
 public class MainActivity extends AppCompatActivity {
 
+    SharedPreferences myPref;
+    public static final String SHARED_PREF="agePref";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        myPref = getSharedPreferences(SHARED_PREF, Activity.MODE_PRIVATE);
+
         setContentView(R.layout.activity_main);
-        System.out.println("Benis");
-        Day tanaan = new(Day(LocalDate))
+        //Day tanaan = new(Day(LocalDate));
+
+        public void onRadioButtonClicked(View view) {
+            // Is the button now checked?
+            boolean checked = ((RadioButton) view).isChecked();
+
+            // Check which radio button was clicked
+            switch(view.getId()) {
+                case R.id.radioButtonMies:
+                    if (checked)
+                        // Pirates are the best
+                        break;
+                case R.id.radioButtonNainen:
+                    if (checked)
+                        // Ninjas rule
+                        break;
+            }
+        }
     }
 
 }
